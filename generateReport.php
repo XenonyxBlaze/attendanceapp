2c9e9bf43d379d0730008c590b3a80a2d92cfa6e
Aarav <aarav.rajput2003@gmail.com>
2023-07-28 02:03:31 +0530

    Report Generation Finalised

--- generateReport.php
+++ generateReport.php
@@ -1,10 +1,6 @@
 <?php
 
-if(!isset($_POST['submit']) && !isset($_POST['date'])) {
-    header('Location: report.html');
-} else {
-    $date = $_POST['date'];
-}
+$date = date("dmY",time());
 
 // SQL config
 $sqlServer = "localhost:3306";
@@ -28,12 +24,46 @@ if(count($tables) == 0) {
     die;
 }
 
-$records = array();
-
 foreach($tables as $table) {
 
-    $createReportQuery = "CREATE TABLE IF NOT EXISTS report".substr($table,8)."(ID VARCHAR(255) NOT NULL PRIMARY KEY, NAME VARCHAR(255), STATUS VARCHAR(255));";
+    $reportTable = "report".substr($table,9);
 
+    $block = substr($table,9,2);
 
+    $createReportQuery = "CREATE TABLE IF NOT EXISTS ".$reportTable."(ID VARCHAR(255) NOT NULL PRIMARY KEY, NAME VARCHAR(255), STATUS VARCHAR(255));";
+
+    $conn->exec($createReportQuery);
+
+    $reportGenQuery = "
+    INSERT INTO $reportTable (
+        ID,
+        Name,
+        Status
+    )
+    SELECT
+        m.ID,
+        m.Name,
+        COALESCE(l.STATUS, CASE WHEN latest_turnstile.STATUS IS NOT NULL THEN latest_turnstile.STATUS ELSE NULL END, 'NEW ENTRY')
+    FROM " . $block . "master AS m
+    LEFT JOIN (
+        SELECT t.ID,
+               CASE
+                   WHEN t.Attendance_Check_Point LIKE '%EXIT%' THEN 'ABSENT'
+                   ELSE 'PRESENT'
+               END AS STATUS
+        FROM $table AS t
+        INNER JOIN (
+            SELECT ID, MAX(Time) AS LatestTime
+            FROM $table
+            GROUP BY ID
+        ) AS latest_time
+        ON t.ID = latest_time.ID AND t.Time = latest_time.LatestTime
+    ) AS latest_turnstile
+    ON m.ID = latest_turnstile.ID
+    LEFT JOIN onleave AS l ON m.ID = l.ID
+    ON DUPLICATE KEY UPDATE Status = VALUES(Status);
+    ";
+
+    echo $reportGenQuery."<br>";
 }
 
